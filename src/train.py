d2b8dac6a6b34d40802ae273f79b916239d9ce33
nick <st102441@student.sbpu.ru>
2023-12-12 12:30:40 +0300

    test_commit

--- src/train.py
+++ src/train.py
@@ -42,21 +42,21 @@ ss = StandardScaler()
 X_train = ss.fit_transform(X_train)
 X_test = ss.transform(X_test)
     
-with Live() as live:
-    live.log_param("n_estimators", n_estimators)
-    clf = RandomForestClassifier(n_estimators=n_estimators)
-    clf.fit(X_train, y_train.values.ravel())
+# with Live() as live:
+#     live.log_param("n_estimators", n_estimators)
+#     clf = RandomForestClassifier(n_estimators=n_estimators)
+#     clf.fit(X_train, y_train.values.ravel())
 
-    y_train_pred = clf.predict(X_train)
+#     y_train_pred = clf.predict(X_train)
 
-    live.log_metric("train/f1", f1_score(y_train, y_train_pred, average="weighted"), plot=False)
-    live.log_sklearn_plot(
-        "confusion_matrix", y_train, y_train_pred, name="train/confusion_matrix",
-        title="Train Confusion Matrix")
+#     live.log_metric("train/f1", f1_score(y_train, y_train_pred, average="weighted"), plot=False)
+#     live.log_sklearn_plot(
+#         "roc", y_train, y_train_pred, name="train/confusion_matrix",
+#         title="Train Confusion Matrix")
 
-    y_test_pred = clf.predict(X_test)
+#     y_test_pred = clf.predict(X_test)
 
-    live.log_metric("test/f1", f1_score(y_test, y_test_pred, average="weighted"), plot=False)
-    live.log_sklearn_plot(
-        "confusion_matrix", y_test, y_test_pred, name="test/confusion_matrix",
-        title="Test Confusion Matrix")
+#     live.log_metric("test/f1", f1_score(y_test, y_test_pred, average="weighted"), plot=False)
+#     live.log_sklearn_plot(
+#         "roc", y_test, y_test_pred, name="test/confusion_matrix",
+#         title="Test Confusion Matrix")
